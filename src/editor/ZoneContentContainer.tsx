0fe1a1385759ae6d10d130caf1441bc8938e7892
Max Bendick <maxbendick@gmail.com>
2020-11-24 18:42:24 -0800

    Separate editor from lib

--- src/editor/ZoneContentContainer.tsx
+++ src/editor/ZoneContentContainer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import * as Config from '../../config'
+import * as Config from '../config'
 
 export const ZoneContentContainer: React.FC<{ numLines: number }> = ({
   numLines,
