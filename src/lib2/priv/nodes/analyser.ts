90b336bd86841a2c0a104afea5ec8f257ebb787a
Max Bendick <maxbendick@gmail.com>
2020-12-06 18:41:08 -0800

    Analyser outputs frequency to console

--- src/lib2/priv/nodes/analyser.ts
+++ src/lib2/priv/nodes/analyser.ts
@@ -10,6 +10,8 @@ type Inputs = { source: AudioNode }
 
 const nodeType = 'testing/analyser'
 
+const BUFFER_LENGTH = 2048
+
 export const superAnalyserDef = {
   nodeType: nodeType,
   inputs: { source: EdgeType.AudioSignal },
@@ -19,20 +21,76 @@ export const superAnalyserDef = {
   argsToConfig: (...[source]: Args): Config => ({}),
   makeOutput: (audioContext: AudioContext, config: Config, inputs: Inputs) => {
     const analyser = audioContext.createAnalyser()
-    analyser.fftSize = 2048
+    analyser.fftSize = BUFFER_LENGTH
 
     inputs.source.connect(analyser)
 
-    // TODO unsubscribe
     const subscription = interval(2000).subscribe(() => {
-      const FFTData = new Float32Array(analyser.frequencyBinCount)
-      analyser.getFloatFrequencyData(FFTData)
-      console.log('fftdata', FFTData[0])
+      console.log(autoCorrelate(audioContext, analyser))
     })
 
-    // TODO unsubscribe
     return { output: 'nothing' as const, subscription }
   },
 } as const
 
 const _proof: SuperDef = superAnalyserDef
+
+function autoCorrelate(audioContext: AudioContext, analyser: AnalyserNode) {
+  var buffer = new Float32Array(BUFFER_LENGTH)
+  analyser.getFloatTimeDomainData(buffer)
+
+  // Implements the ACF2+ algorithm
+  var SIZE = buffer.length
+  var rms = 0
+
+  for (var i = 0; i < SIZE; i++) {
+    var val = buffer[i]
+    rms += val * val
+  }
+  rms = Math.sqrt(rms / SIZE)
+  if (rms < 0.01)
+    // not enough signal
+    return -1
+
+  var r1 = 0,
+    r2 = SIZE - 1,
+    thres = 0.2
+  for (var i = 0; i < SIZE / 2; i++)
+    if (Math.abs(buffer[i]) < thres) {
+      r1 = i
+      break
+    }
+  for (var i = 1; i < SIZE / 2; i++)
+    if (Math.abs(buffer[SIZE - i]) < thres) {
+      r2 = SIZE - i
+      break
+    }
+
+  buffer = buffer.slice(r1, r2)
+  SIZE = buffer.length
+
+  var c = new Array(SIZE).fill(0)
+  for (var i = 0; i < SIZE; i++)
+    for (var j = 0; j < SIZE - i; j++) c[i] = c[i] + buffer[j] * buffer[j + i]
+
+  var d = 0
+  while (c[d] > c[d + 1]) d++
+  var maxval = -1,
+    maxpos = -1
+  for (var i = d; i < SIZE; i++) {
+    if (c[i] > maxval) {
+      maxval = c[i]
+      maxpos = i
+    }
+  }
+  var T0 = maxpos
+
+  var x1 = c[T0 - 1],
+    x2 = c[T0],
+    x3 = c[T0 + 1]
+  const a = (x1 + x3 - 2 * x2) / 2
+  const b = (x3 - x1) / 2
+  if (a) T0 = T0 - b / (2 * a)
+
+  return audioContext.sampleRate / T0
+}
