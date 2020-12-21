9200403a16d8e5195dd0a1acbacd780cbf84dec3
Max Bendick <maxbendick@gmail.com>
2020-12-21 12:02:51 -0800

    Dial in user-land

--- public/default-files/dial.tsx
+++ public/default-files/dial.tsx
@@ -1,61 +1,242 @@
+import { useObservableState } from 'observable-hooks'
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { getAudioContext, interactable } from './internal'
+import { useEffect, useRef } from 'react'
+import { useDrag } from 'react-use-gesture'
+import { Observable } from 'rxjs'
+import { map, sampleTime, scan } from 'rxjs/operators'
+import styled from 'styled-components'
 
-for (let i = 0; i < 10; i++) {
-  // do something
+interface Movement {
+  down: boolean
+  movement: [mx: number, my: number]
+}
+type DialState = {
+  preDownValue: number
+  currValue: number
+  prevMovement: Movement
 }
 
-export const nodeHandler = domNode =>
-  ReactDOM.render(<div>hello dddreact</div>, domNode)
-
-const myDomNode = document.createElement('div')
-nodeHandler(myDomNode)
-
-export const another = interactable({
-  value: ['asdkjfjskdfmnvn'],
-  domNode: myDomNode,
-  onDestroy: () => {
-    ReactDOM.unmountComponentAtNode(myDomNode)
-  },
+const initialDialState = (initialValue: number): DialState => ({
+  preDownValue: initialValue, // TODO inject default
+  currValue: initialValue, // TODO inject default
+  prevMovement: { down: false, movement: [0, 0] },
 })
 
-const c = getAudioContext() as AudioContext
-const masterOut = c.createOscillator()
-masterOut.start()
+const DIAL_MIN_VALUE = -75
+const DIAL_MAX_VALUE = 75
 
-export default masterOut
+const dialReducer = (state: DialState, movement: Movement): DialState => {
+  const {
+    movement: [, my],
+  } = movement
 
-console.log('audiocontext', getAudioContext())
+  const deltaY = (movement as any)?.event?.movementY ?? 0
 
-const reactInteractable = (value, Component) => {
-  const domNode = document.createElement('div')
-  ReactDOM.render(<Component />, domNode)
+  const currDown = movement.down
+  const prevDown = state.prevMovement.down
 
-  const parent = document.createElement('div')
+  const inBounds = (value: number) =>
+    Math.max(DIAL_MIN_VALUE, Math.min(DIAL_MAX_VALUE, value))
 
-  parent.setAttribute('style', 'width: 500px; height: 71px; background: black')
+  const nextValue = inBounds(state.currValue + deltaY)
 
-  parent.appendChild(domNode)
+  if (prevDown && !currDown) {
+    // mouse up
+    return {
+      ...state,
+      currValue: nextValue,
+      preDownValue: nextValue,
+      prevMovement: movement,
+    }
+  }
+  if (!prevDown && currDown) {
+    // mouse down
+    return {
+      ...state,
+      prevMovement: movement,
+      currValue: nextValue,
+    }
+  }
+  if (currDown) {
+    // just a move
+    return {
+      ...state,
+      prevMovement: movement,
+      currValue: nextValue,
+    }
+  }
 
-  console.log('reactInteractable parent', parent)
-
-  return interactable({
-    value,
-    domNode: parent,
-    onDestroy: () => {
-      ReactDOM.unmountComponentAtNode(domNode)
-    },
-  })
+  return state
 }
 
-// solution:
-//  value must return synchronously
-//  can have promise for node?
-//  can create empty node at start, then fill it!!
+const translateDialValue = (value: number, start: number, end: number) => {
+  const normalized =
+    (1.0 * (-1.0 * value - DIAL_MIN_VALUE)) / (DIAL_MAX_VALUE - DIAL_MIN_VALUE)
 
-export const aReactInteractable = reactInteractable({ some: 'value' }, () => (
-  <div>hello asdfasdfinteractable</div>
-))
+  return normalized * (end - start) + start
+}
 
-console.log('value of aReactInteractable', aReactInteractable)
+const movementsToDialValue = (
+  start: number,
+  end: number,
+  initialValue: number,
+  sampleRate: number,
+) => (
+  event$: Observable<{ down: boolean; movement: [mx: number, my: number] }>,
+): Observable<{
+  value: number
+  dragging: boolean
+}> => {
+  return event$.pipe(
+    scan(dialReducer, initialDialState(initialValue)),
+    sampleTime(sampleRate),
+    map(state => {
+      return {
+        value: translateDialValue(state.currValue, start, end),
+        dragging: state.prevMovement.down,
+      }
+    }),
+  )
+}
+
+const DialBase = styled.div<{ radius: number }>`
+  height: ${props => props.radius * 2}px;
+  width: ${props => props.radius * 2}px;
+  border-radius: ${props => props.radius * 2}px;
+  background-color: #000;
+  user-select: none;
+`
+
+const DialTickContainer = styled.div<{
+  transitions: boolean
+  hide: boolean
+  radius: number
+}>`
+  height: ${props => props.radius * 2}px;
+  width: ${props => props.radius * 2}px;
+  position: absolute;
+  transition: transform 0.1s, filter 0.4s;
+  display: flex;
+  justify-content: center;
+  filter: opacity(${props => (props.hide ? '0' : '1')});
+`
+const DialTickInner = styled.div<{
+  thickness: string
+  color: string
+  length: string
+}>`
+  height: ${props => props.length};
+  width: ${props => props.thickness};
+  background-color: ${props => props.color};
+`
+
+interface TickProps {
+  color: any
+  degrees: number
+  moveable: boolean
+  hide: boolean
+  length: any
+  radius: number
+  thickness: string
+}
+const DialTick: React.FC<TickProps> = ({
+  color,
+  degrees,
+  moveable,
+  hide,
+  length,
+  radius,
+  thickness,
+}) => (
+  <DialTickContainer
+    style={{ transform: `rotate(${degrees}deg)` }}
+    transitions={moveable}
+    hide={hide}
+    radius={radius}
+  >
+    <DialTickInner color={color} length={length} thickness={thickness} />
+  </DialTickContainer>
+)
+
+const normalize = (start: number, end: number, value: number) =>
+  (value - start) / (end - start)
+
+export const Dial: React.FC<{
+  initialValue: number
+  start: number
+  end: number
+  send: (a: number) => void
+  radius: number
+  sampleRate?: number // 100 by default
+}> = ({ send, start, end, initialValue, radius, sampleRate }) => {
+  const dialBaseElement = useRef(null)
+
+  const [{ value, dragging }, registerMovement] = useObservableState(
+    movementsToDialValue(start, end, initialValue, sampleRate || 100),
+    {
+      value: initialValue,
+      dragging: false,
+    },
+  )
+
+  useEffect(() => {
+    const element = (dialBaseElement.current as any) as HTMLElement
+    if (dragging) {
+      element.requestPointerLock()
+    } else {
+      document.exitPointerLock()
+    }
+  }, [dragging])
+
+  useEffect(() => {
+    try {
+      send(value)
+    } catch (e) {
+      console.warn('sent with an error - send may be undefined')
+    }
+  }, [send, value])
+
+  const bind = useDrag(registerMovement)
+
+  // 240 degrees of movement, centered at 0
+  const span = 240
+  const startDegrees = span / -2.0
+  const endDegrees = span / 2.0
+  const degrees = normalize(start, end, value) * span + startDegrees
+
+  const showThreshold = 50
+  const showMin = dragging && degrees - startDegrees < showThreshold
+  const showMax = dragging && endDegrees - degrees < showThreshold
+
+  return (
+    <DialBase radius={radius} {...bind()} ref={dialBaseElement}>
+      <DialTick
+        color={'#779'}
+        degrees={startDegrees}
+        moveable={false}
+        hide={!showMin}
+        length={'20%'}
+        radius={radius}
+        thickness={'2px'}
+      />
+      <DialTick
+        color={'#779'}
+        degrees={endDegrees}
+        moveable={false}
+        hide={!showMax}
+        length={'20%'}
+        radius={radius}
+        thickness={'2px'}
+      />
+      <DialTick
+        color={'#13d'}
+        degrees={degrees}
+        moveable={true}
+        hide={false}
+        length={'50%'}
+        radius={radius}
+        thickness={'3px'}
+      />
+    </DialBase>
+  )
+}
--- public/default-files/index.tsx
+++ public/default-files/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Dial } from './dial'
 import { getAudioContext, interactable } from './internal'
 
 for (let i = 0; i < 10; i++) {
   // do something
 }
+
+console.log('the dial component fn', Dial)
 
 export const nodeHandler = domNode =>
   ReactDOM.render(<div>hello dddreact</div>, domNode)
@@ -30,7 +33,10 @@ const reactInteractable = (value, Component) => {
 
   const parent = document.createElement('div')
 
-  parent.setAttribute('style', 'width: 500px; height: 71px; background: black')
+  parent.setAttribute(
+    'style',
+    'width: 500px; height: 71px; border: 1px solid black',
+  )
 
   parent.appendChild(domNode)
 
@@ -51,7 +57,11 @@ const reactInteractable = (value, Component) => {
 //  can create empty node at start, then fill it!!
 
 export const aReactInteractable = reactInteractable({ some: 'value' }, () => (
-  <div>hello asdfasdfinteractable</div>
+  <div>
+    hello asdfasdfinteractable
+    <br />
+    <Dial start={0} end={100} initialValue={50} send={() => {}} radius={20} />
+  </div>
 ))
 
 console.log('value of aReactInteractable', aReactInteractable)
